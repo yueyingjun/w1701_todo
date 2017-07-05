82c901f09d93e1fdad5d6fb16834d2644df2007c
bloodkey <3028336816@qq.com>
2017-07-05 13:53:41 +0800

    'zhongwu'

--- remember/remember.js
+++ remember/remember.js
@@ -7,7 +7,7 @@ angular.module("myapp",[])
         /*添加列表*/
         $scope.add=function(){
             var obj={};
-            obj.id=maxid();
+            obj.id=maxid($scope.data);
             obj.name="新建存档"+obj.id;
             obj.son=[];
             $scope.data.push(obj);
@@ -15,16 +15,16 @@ angular.module("myapp",[])
             $scope.currentid=obj.id;
         }
 
-        function maxid(){
+        function maxid(arr){
             var id=0;
             var tempid=0;
-            if($scope.data.length==0){
+            if(arr.length==0){
                     id=1;
             }else{
 
-                  for(var i=0;i<$scope.data.length;i++){
-                    if($scope.data[i].id>tempid){
-                        tempid=$scope.data[i].id
+                  for(var i=0;i<arr.length;i++){
+                    if(arr[i].id>tempid){
+                        tempid=arr[i].id
 
                     }
                   }
@@ -33,7 +33,7 @@ angular.module("myapp",[])
             }
             return id;
         }
-        $scope.currentid=1;
+        $scope.currentid=$scope.data[0].id;
         $scope.setid=function(currentid){
             $scope.currentid=currentid;
         }
@@ -49,6 +49,41 @@ angular.module("myapp",[])
         }
         $scope.save=function(){
             localStorage.message=JSON.stringify($scope.data);
+        }
+        $scope.del=function(id){
+            if(confirm('是否删除该存档？？？')){
+                // alert(1)
+                for(let i=0;i<$scope.data.length;i++){
+                    if($scope.data[i].id==id){
+                        $scope.data.splice(i,1);
+                        if($scope.currentid==id){
+                            $scope.currentid=$scope.data[0].id;
+                        }
+                    }
+                }
+
+            localStorage.message=JSON.stringify($scope.data);
+            }else{
+                // alert(2)
+
+            }
+        }
+        // 右边
+        $scope.addcon=function(id){
+            let index;
+            $.each($scope.data,function(k,v){
+                if(v.id==id){
+                    index=k;
+                    // break;
+                }
+            })
+            var obj={};
+            obj.id=maxid($scope.data[index].son);
+            obj.con="早起的虫子有鸟吃！！！！"+obj.id;
+           
+            $scope.data[index].son.push(obj);
+            localStorage.message=JSON.stringify($scope.data);
+            // $scope.currentid=obj.id;
         }
 
 
