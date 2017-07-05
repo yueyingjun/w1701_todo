484d871f706ef63ee8e7e983f1f90b48bdf7fae2
叔叔不喝茶 <1129476352@qq.com>
2017-07-05 08:58:02 +0800

    11

--- to_do/to_do.js
+++ to_do/to_do.js
@@ -1,7 +1,7 @@
 angular.module("myapp",[])
     .controller("todo",["$scope",function($scope){
         $scope.data=localStorage.message?JSON.parse(localStorage.message):[];
-        console.log($scope.data)
+        // console.log($scope.data)
         /*添加列表*/
         $scope.add=function(){
             var obj={};
@@ -11,26 +11,41 @@ angular.module("myapp",[])
             $scope.data.push(obj);
             localStorage.message=JSON.stringify($scope.data);
         }
-
+        $scope.change=function(){
+            var obj={};
+            obj.id=maxid();
+            obj.name="新建列表";
+            obj.son=[];
+            $scope.data.push(obj);
+            $scope.data.pop();
+            localStorage.message=JSON.stringify($scope.data);
+        }
+        // $scope.change=function(id){
+        //     for(var i=0;i<$scope.data.length;i++){
+        //        if(localStorage.message.id==id){
+        //            localStorage.message.name=$scope.data.name;
+        //        }
+        //     }
+        //     // $scope.data.name = $scope.item.name;
+        //
+        // }
+        
         function maxid(){
             var id=0;
             var tempid=0;
             if($scope.data.length==0){
-                    id=1;
+                id=1;
             }else{
 
-                  for(var i=0;i<$scope.data.length;i++){
+                for(var i=0;i<$scope.data.length;i++){
                     if($scope.data[i].id>tempid){
                         tempid=$scope.data[i].id
 
                     }
-                  }
+                }
 
-                  id=tempid+1;
+                id=tempid+1;
             }
             return id;
         }
-
-
-
-}])
+    }])
