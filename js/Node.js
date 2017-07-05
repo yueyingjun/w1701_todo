38a8cfd10e721c4d3b8893fa2f9e09f32b5b480a
cure-qiao <351306223@qq.com>
2017-07-05 17:09:40 +0800

    dd

--- js/Node.js
+++ js/Node.js
@@ -2,35 +2,123 @@
  * Created by chen on 2017/7/5.
  */
 angular.module("myapp",[])
-    .controller("ctrl",["$scope",function($scope){
+    .controller("ctrl",["$scope","$filter",function($scope,$filter){
         $scope.data=localStorage.message?JSON.parse(localStorage.message):[];
+        $scope.com_Arr=localStorage.done?JSON.parse(localStorage.done):[];
+        $scope.comArrlen=$scope.com_Arr.length;
+        $scope.flag=true;
         $scope.add=function(){
             var obj={};
-            obj.id=maxid();
+            obj.id=maxid($scope.data);
             obj.name="未标签";
             obj.son=[];
             $scope.data.push(obj);
             localStorage.message=JSON.stringify($scope.data);
+            $scope.currentIndex=getIndex($scope.data,obj.id);
+            $scope.currentObj=$scope.data[$scope.currentIndex];
+        }
+        //删除 列表
+        $scope.del=function () {
+            var currentIndex=getIndex($scope.data,this.item.id);
+            $scope.data.splice(currentIndex,1);
+            $scope.currentIndex=getIndex($scope.data,$scope.data.length);
+            $scope.currentObj=$scope.data[$scope.currentIndex];
+            localStorage.message=JSON.stringify($scope.data);
+        }
+        //删除内容
+        $scope.delCon=function () {
+            var sonIndex=getIndex($scope.currentSon,this.item.id);
+            $scope.currentSon.splice(sonIndex,1);
+            localStorage.message=JSON.stringify($scope.data);
+        }
+        //删除已完成事项
+        $scope.delComCon=function () {
+            var comIndex=getIndex($scope.com_Arr,this.item.id);
+            console.log(comIndex);
+            $scope.com_Arr.splice(comIndex,1);
+            localStorage.done=JSON.stringify($scope.completeArr);
         }
 
-        function maxid(){
+        //让一开始的时候页面显示左后一个的内容
+        $scope.currentIndex=getIndex($scope.data,$scope.data.length);
+        $scope.currentObj=$scope.data[$scope.currentIndex];
+
+        //获取焦点
+       $scope.focus=function () {
+           $scope.currentIndex=getIndex($scope.data,this.item.id);
+           $scope.currentObj=$scope.data[$scope.currentIndex];
+           $scope.currentSon=$scope.currentObj.son;
+           if($scope.flag==false){
+               $scope.flag=true;
+           }
+           // console.log($scope.currentSon);
+       }
+       // console.log($scope.currentObj);
+       //  $scope.currentSon=$scope.currentObj.son;
+       //失去焦点
+       $scope.blur=function () {
+           localStorage.message=JSON.stringify($scope.data);
+       }
+       //添加内容
+       $scope.addCon=function () {
+           $scope.currentSon=$scope.currentObj.son;
+            $scope.sonObj={
+                    id:maxid($scope.currentSon),
+                    name:"新建内容",
+                    listname:$scope.currentSon.name,
+                };
+           $scope.currentSon.push($scope.sonObj);
+           localStorage.message=JSON.stringify($scope.data);
+       }
+       //完成内容
+        $scope.completeArr=[];
+        $scope.completeCon=function () {
+            $scope.arrIndex=getIndex($scope.currentSon,this.item.id);
+            $scope.arrSplice=$scope.currentSon.splice($scope.arrIndex,1);
+            $scope.completeArr.push($scope.arrSplice[0]);
+            localStorage.message=JSON.stringify($scope.data);
+            localStorage.done=JSON.stringify($scope.completeArr);
+        }
+        $scope.comArrlen=$scope.com_Arr.length;
+        $scope.date = $filter('date')(new Date());
+        // 搜索
+        $scope.search="";
+        $scope.$watch("search",function () {
+            $scope.searchArr=$filter("filter","search")($scope.data,{name:$scope.search});
+            $scope.searchlen=$scope.searchArr.length;
+            $scope.currentIndex=getIndex($scope.data,$scope.searchlen);
+            $scope.currentObj=$scope.data[$scope.currentIndex];
+        });
+        $scope.showcom=function () {
+           if($scope.flag==true){
+               $scope.flag=false;
+           }else{
+               $scope.flag=true;
+           }
+        }
+        function maxid(arr){
             var id=0;
             var tempid=0;
-            if($scope.data.length==0){
+            if(arr.length==0){
                 id=1;
             }else{
-                for(var i=0;i<$scope.data.length;i++){
-                    if($scope.data[i].id>tempid){
-                        tempid=$scope.data[i].id
+                for(var i=0;i<arr.length;i++){
+                    if(arr[i].id > tempid){
+                        tempid=arr[i].id
                     }
                 }
                 id=tempid+1;
             }
             return id;
         }
-        $scope.del=function () {
-            $scope.data.splice(this.item,1);
-            localStorage.message=JSON.stringify($scope.data);
+        //得到当前的id
+        function getIndex(arr,id){
+            for(var i=0;i<arr.length;i++){
+                if(id==arr[i].id){
+                   return i;
+                }
+            }
         }
+
     }])
 
