5b7c1422d9b5c0a4ec68f00aa28a20e5f2562f27
buise <1078688845@qq.com>
2017-07-06 09:34:39 +0800

    'fh'

--- node/message.js
+++ node/message.js
@@ -1,159 +1,116 @@
 /**
  * Created by Administrator on 2017/7/4.
  */
-    angular.module('note',[])
-        .controller('ctrl',["$scope",function ($scope) {
-            $scope.data=localStorage.message?JSON.parse(localStorage.message):[];
-            $scope.now=0;
-            $scope.all=0;
-            // 添加
-            $scope.add=function () {
-                var time=new Date();
-                var obj={};
-                obj.id=maxid();
-                obj.name="新建列表";
-                obj.son=[];
-                obj.time=time.getTime()
-                $scope.data.push(obj);
-                localStorage.message=JSON.stringify($scope.data);
-                $scope.now=obj.id-1;
-            }
+angular.module('note',[])
+    .controller('ctrl',['$scope','$filter',function ($scope,$filter) {
+        $scope.data=localStorage.message?JSON.parse(localStorage.message):[];
+        $scope.down=localStorage.down?JSON.parse(localStorage.down):[];
+        $scope.currentIndex=0;
+        $scope.currentData=$scope.data[$scope.currentIndex];
+        $scope.flag=true;
+        $scope.sou='';
 
-            $scope.del=function (id) {
-                angular.forEach($scope.data,function (val, key) {
-                    if($scope.data[key].id==id){
-                        $scope.data.splice(key,1);
-                        localStorage.message=JSON.stringify($scope.data);
-                    }
-                })
-            }
+        // 搜索
+        $scope.$watch("sou",function (news) {
+            let arr=$filter('filter')($scope.data,{name:news})
+            $scope.currentData=arr[arr.length-1];
+        })
 
-            $scope.gaibian=function (id) {
-                angular.forEach($scope.data,function (val, key) {
-                    if($scope.data[key].id==id){
-                        var aa=JSON.parse(localStorage.message);
-                        aa[key].name=$scope.data[key].name
-                        localStorage.message=JSON.stringify(aa);
-                    }
-                })
-            }
+        // 添加事项
+        $scope.add=function () {
+            $scope.flag=true;
+            var obj={};
+            obj.id=maxId($scope.data);
+            obj.name='新建备忘录'+obj.id;
+            obj.time=new Date();
+            obj.son=[];
+            $scope.data.push(obj);
+            localStorage.message=JSON.stringify($scope.data);
+            $scope.currentIndex=$scope.data.length-1;
+            $scope.currentData=$scope.data[$scope.currentIndex];
+        }
 
-            $scope.fn=function (n) {
-                $scope.now = n;
-            }
+        // 改变事项名称
+        $scope.change=function () {
+            $scope.flag=true;
+            localStorage.message=JSON.stringify($scope.data);
+        }
 
-            $scope.addson=function (id) {
-                var obj={};
-                obj.id=minid(id);
-                obj.con="填写备忘录";
-                obj.ok=0;
-                angular.forEach($scope.data,function (val, key){
-                    if($scope.data[key].id==id){
-                        $scope.data[key].son.push(obj);
-                    }
-                })
-                localStorage.message=JSON.stringify($scope.data);
-            }
+        // 删除事项
+        $scope.del=function (id) {
+            var index=geti($scope.data,id);
+            $scope.data.splice(index,1);
+            localStorage.message=JSON.stringify($scope.data);
+        }
 
-            $scope.delson=function (pid,id) {
-                var keyid2;
-                angular.forEach($scope.data,function (val, key){
-                    if($scope.data[key].id==pid){
-                        keyid2=key;
-                    }
-                })
-                for(var i=0;i<$scope.data[keyid2].son.length;i++){
-                    if($scope.data[keyid2].son[i].id==id){
-                        $scope.data[keyid2].son.splice(i,1);
-                        localStorage.message=JSON.stringify($scope.data);
-                    }
-                }
-            }
+        // 点击事项
+        $scope.fn=function (id) {
+            $scope.flag=true;
+            var index=geti($scope.data,id);
+            $scope.currentIndex=index;
+            $scope.currentData=$scope.data[$scope.currentIndex];
+        }
 
-            $scope.gaibianson=function (pid,id) {
-                var keyid2;
-                angular.forEach($scope.data,function (val, key){
-                    if($scope.data[key].id==pid){
-                        keyid2=key;
-                    }
-                })
-                for(var i=0;i<$scope.data[keyid2].son.length;i++){
-                    if($scope.data[keyid2].son[i].id==id){
-                        var aa=JSON.parse(localStorage.message);
-                        aa[keyid2].son[i].con=$scope.data[keyid2].son[i].con
-                        localStorage.message=JSON.stringify(aa);
-                    }
-                }
-            }
+        // 添加备忘事件
+        $scope.addson=function () {
+            var obj={};
+            obj.id=maxId($scope.currentData.son);
+            obj.con='新建备忘事件'+obj.id;
+            obj.ok=0;
+            $scope.currentData.son.push(obj);
+            localStorage.message=JSON.stringify($scope.data);
+        }
 
-            $scope.backson=function (pid,id) {
-                var keyid2;
-                angular.forEach($scope.data,function (val, key){
-                    if($scope.data[key].id==pid){
-                        keyid2=key;
-                    }
-                })
-                for(var i=0;i<$scope.data[keyid2].son.length;i++){
-                    if($scope.data[keyid2].son[i].id==id){
-                        $scope.data[keyid2].son[i].ok=1;
-                        var aa=JSON.parse(localStorage.message);
-                        aa[keyid2].son[i].ok=1;
-                        localStorage.message=JSON.stringify(aa);
-                        $scope.all++;
+        // 删除备忘事件
+        $scope.delson=function (id) {
+            var index=geti($scope.currentData.son,id);
+            $scope.currentData.son.splice(index,1);
+            localStorage.message=JSON.stringify($scope.data);
+            var index2=geti($scope.down,id);
+            $scope.down.splice(index2,1);
+            localStorage.down=JSON.stringify($scope.down);
+        }
+
+        // 完成备忘事件
+        $scope.over=function (id) {
+            var index=geti($scope.currentData.son,id);
+            $scope.currentData.son[index].ok=1;
+            localStorage.message=JSON.stringify($scope.data);
+            var obj={};
+            obj.id=$scope.currentData.id+"-"+$scope.currentData.son[index].id;
+            obj.par=$scope.currentData.name;
+            obj.con=$scope.currentData.son[index].con;
+            $scope.down.push(obj);
+            localStorage.down=JSON.stringify($scope.down);
+        }
+
+        // 显示完成的备忘事件
+        $scope.ok=function () {
+            $scope.flag=false;
+        }
+        // 获取当前数组中的最大id
+        function maxId(arr) {
+            var id=0;
+            if(arr.length==0){
+                id=1;
+            }else{
+                for(var i=0;i<arr.length;i++){
+                    if(arr[i].id>=id){
+                        id=arr[i].id+1;
                     }
                 }
             }
+            return id;
+        }
 
-            for(var i=0;i<$scope.data.length;i++){
-                for(var j=0;j<$scope.data[i].son.length;j++){
-                    if($scope.data[i].son[j].ok==1){
-                        $scope.all++;
-                    }
+        // 判断当前id在数组中是第几个
+        function geti(arr, pid) {
+            var key;
+            angular.forEach(arr,function (obj, i) {
+                if (obj.id==pid){
+                    key=i;
                 }
-            }
-
-
-
-
-            function maxid(){
-                var id=0;
-                var tempid=0;
-                if($scope.data.length==0){
-                    id=1;
-                }else{
-
-                    for(var i=0;i<$scope.data.length;i++){
-                        if($scope.data[i].id>tempid){
-                            tempid=$scope.data[i].id;
-
-                        }
-                    }
-
-                    id=tempid+1;
-                }
-                return id;
-            }
-
-            function minid(pid){
-                var id=0;
-                var tempid=0;
-
-                angular.forEach($scope.data,function (val, key){
-                    if($scope.data[key].id==pid){
-                        keyid=key;
-                    }
-                })
-
-                if($scope.data[keyid].son.length==0){
-                    id=1;
-                }else{
-                    for(var i=0;i<$scope.data[keyid].son.length;i++){
-                        if($scope.data[keyid].son[i].id>tempid){
-                            tempid=$scope.data[keyid].son[i].id;
-                        }
-                    }
-                    id=tempid+1;
-                }
-                return id;
-            }
-        }])
+            })
+            return key;
+        }
+    }])
