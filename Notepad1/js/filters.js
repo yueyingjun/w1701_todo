95b33b2ed8d7493dbc862710f8a10db88eaedf51
zhengweipx <zhengweipx@163.com>
2017-07-06 09:07:40 +0800

    '1'

---
+++ Notepad1/js/filters.js
@@ -0,0 +1,27 @@
+angular.module("filters",[])
+	.filter('maxOrmin',function(arr,type){
+		return function(arr){
+			if (angular.isArray(arr)) {
+				var type=type;
+				if (type==1) {
+					var temp=arr[0];
+					for (var i = 0; i < arr.length; i++) {
+						if (temp<arr[i]) {
+							temp=arr[i];
+						}
+					}
+				}else{
+					for (var i = 0; i < arr.length; i++) {
+						if (temp>arr[i]) {
+							temp=arr[i];
+						}
+					}
+					return temp;
+				}
+			}else{
+				console.error("请输入数组");
+			}
+		}
+		
+		
+	})
