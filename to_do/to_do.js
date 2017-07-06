842d67b1d4dd598f4ce4b1a25d844141bb663355
叔叔不喝茶 <1129476352@qq.com>
2017-07-06 09:26:39 +0800

    11

--- to_do/to_do.js
+++ to_do/to_do.js
@@ -1,50 +1,118 @@
-angular.module("myapp",[])
-    .controller("todo",["$scope",function($scope){
-        $scope.data=localStorage.message?JSON.parse(localStorage.message):[];
-        // console.log($scope.data)
+angular.module("myapp", [])
+    .controller("todo", ["$scope", function ($scope) {
+        $scope.data = localStorage.message ? JSON.parse(localStorage.message):[];
+        $scope.currentIndex = $scope.data.length - 1;
+        $scope.current = $scope.data[$scope.currentIndex];
+        $scope.done = localStorage.done ? JSON.parse(localStorage.done) : [];
+        $scope.show = true;
         /*添加列表*/
-        $scope.add=function(){
-            var obj={};
-            obj.id=maxid();
-            obj.name="新建列表";
-            obj.son=[];
+        $scope.add = function () {
+            $scope.show = true;
+            var obj = {};
+            obj.id = maxid();
+            obj.name = "新建列表" + obj.id;
+            obj.son = [];
             $scope.data.push(obj);
-            localStorage.message=JSON.stringify($scope.data);
-        }
-        $scope.change=function(){
-            var obj={};
-            obj.id=maxid();
-            obj.name="新建列表";
-            obj.son=[];
-            $scope.data.push(obj);
-            $scope.data.pop();
-            localStorage.message=JSON.stringify($scope.data);
-        }
-        // $scope.change=function(id){
-        //     for(var i=0;i<$scope.data.length;i++){
-        //        if(localStorage.message.id==id){
-        //            localStorage.message.name=$scope.data.name;
-        //        }
-        //     }
-        //     // $scope.data.name = $scope.item.name;
-        //
-        // }
-        
-        function maxid(){
-            var id=0;
-            var tempid=0;
-            if($scope.data.length==0){
-                id=1;
-            }else{
+            $scope.currentIndex = getIndex($scope.data, obj.id);
+            $scope.current = $scope.data[$scope.currentIndex];
+            localStorage.message = JSON.stringify($scope.data);
 
-                for(var i=0;i<$scope.data.length;i++){
-                    if($scope.data[i].id>tempid){
-                        tempid=$scope.data[i].id
+        }
+        $scope.focus=function (id,up,son){
+            $scope.show=true;
+            $scope.currentIndex=getIndex($scope.data,id);
+            $scope.current=$scope.data[$scope.currentIndex];
+        }
+        $scope.blur = function () {
+            localStorage.message = JSON.stringify($scope.data);
+        }
+        //删除列表
+        $scope.del = function (id) {
+            var ind = getIndex($scope.data, id);
+            angular.forEach($scope.data, function (obj, index) {
+                if (id == obj.id) {
+                    // $scope.data.splice(index, 1);
+                    if (ind == $scope.data.length - 1) {
+                        $scope.currentIndex = ind - 1;
+                        $scope.current = $scope.data[$scope.currentIndex];
+                        $scope.data.splice(ind, 1);
+                        localStorage.message = JSON.stringify($scope.data);
+                    } else {
+                        $scope.currentIndex = $scope.data.length - 1;
+                        $scope.current = $scope.data[$scope.currentIndex];
+                        $scope.data.splice(ind, 1);
+                        localStorage.message = JSON.stringify($scope.data);
+                    }
+                }
+            })
+        }
+        //添加子内容
+        $scope.addOpt = function () {
+            var obj = {};
+            obj.id = maxid($scope.current.son);
+            obj.name = "新建条目" + obj.id;
+            $scope.current.son.push(obj);
+            localStorage.message = JSON.stringify($scope.current.son);
+            // localStorage.done = JSON.stringify($scope.done);
+        }
+        //点击完成
+        $scope.success = function (id) {
+            var index=$scope.currentIndex;
+            var obj={};
+            obj.id=maxid($scope.done);
+            obj.name=$scope.data[index].name;
+            obj.opt=$scope.current.son[getIndex($scope.current.son,id)].name;
+            $scope.done.push(obj);
+            localStorage.done = JSON.stringify($scope.done);
+            $scope.current.son.splice(getIndex($scope.current.son,id),1);
+            localStorage.message = JSON.stringify($scope.data);
+        }
+        //显示已完成内容
+        $scope.change = function () {
+            $scope.show = false;
+        }
+        //删除已完成
+        $scope.delDone = function (id) {
+            angular.forEach($scope.done,function (obj,index) {
+                if(id==obj.id){
+                    $scope.done.splice(index,1);
+                    localStorage.done = JSON.stringify($scope.done);
+                }
+            })
+        }
+        //删除 子内容
+        $scope.delCon = function (id) {
+            angular.forEach($scope.current.son,function (obj,index) {
+                if(obj.id==id){
+                    $scope.current.son.splice(index,1);
+                    localStorage.message = JSON.stringify($scope.data);
+                }
+            })
+        }
+        function getIndex(arr, id) {
+            for (var i = 0; i < arr.length; i++) {
+                if (arr[i].id == id) {
+                    return i;
+                }
+            }
+        }
+
+        function maxid(arr) {
+            var arr = arr || $scope.data;
+            var id = 0;
+            var tempid = 0;
+            if (arr.length == 0) {
+                id = 1;
+            } else {
+
+                for (var i = 0; i < arr.length; i++) {
+                    if (arr[i].id > tempid) {
+                        tempid = arr[i].id
 
                     }
                 }
 
-                id=tempid+1;
+                id = tempid + 1;
             }
             return id;
         }
